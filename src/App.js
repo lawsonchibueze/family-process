b9dc0252e14b60bd70c07231edbfc6b8d39170cf
Lawson Chibueze <speak2lawson@gmail.com>
2021-05-15 01:34:39 +0100

    My Third Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useReducer } from 'react';
 
 import awsConfig from './aws-exports';
 import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
@@ -12,14 +12,31 @@ import Lists from './components/Lists/Lists';
 import { Button, Container, Form, Icon, Modal } from 'semantic-ui-react';
 Amplify.configure(awsConfig);
 
+const initialState = {
+	title: '',
+	description: ''
+};
 function App() {
+	function listReducer(state = initialState, action) {
+		switch (action.type) {
+			case 'DESCRIPTION':
+				return { ...state, description: action.value };
+			case 'TITLE_CHANGED':
+				return { ...state, title: action.value };
+			default:
+				console.log('Default action for :', action);
+				return state;
+		}
+	}
 	async function fetchList() {
 		const { data } = await API.graphql(graphqlOperation(listLists));
 		setLists(data.listLists.items);
 		console.log(data);
 	}
+
+	const [ state, dispatch ] = useReducer(listReducer, initialState);
 	const [ lists, setLists ] = useState([]);
-	const [ isModalOpen, setIsModalOpen ] = useState(false);
+	const [ isModalOpen, setIsModalOpen ] = useState(true);
 	useEffect(() => {
 		fetchList();
 	}, []);
@@ -44,8 +61,23 @@ function App() {
 				<Modal.Header>Create yourList</Modal.Header>
 				<Modal.Content>
 					<Form>
-            <Form.Input label='List Title' placeholder='My pretty List' />
-            <Form.TextArea label = "Description" placeholder = "Things that my pretty list is a about"></Form.TextArea>
+						<Form.Input
+							error={
+
+									true ? false :
+									{ Content: 'Please add a name to your title' }
+							}
+							label='List Title'
+							placeholder='My pretty List'
+							value={state.title}
+							onChange={(e) => dispatch({ type: 'TITLE_CHANGED', value: e.target.vaue })}
+						/>
+						<Form.TextArea
+							label='Description'
+							placeholder='Things that my pretty list is a about'
+							value={state.description}
+							onChange={(e) => dispatch({ type: 'DESCRIPTION', value: e.target.vaue })}
+						/>
 					</Form>
 				</Modal.Content>
 				<Modal.Actions>
